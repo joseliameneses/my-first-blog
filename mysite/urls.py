cb0fac422e7ec4e26874404c65a32cf7444163dc
joseliameneses <joselia.meneses@aguasdeteresina.com.br>
2018-05-07 16:06:48 -0300

    Modifield templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
-
+    
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'', include('blog.urls'))
